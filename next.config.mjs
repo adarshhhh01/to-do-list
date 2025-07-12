3ad4b0b7b1c7d896d5f05a2ff1c97cec737539d8
Adarsh namdeo <adarshnamdeo73@gmail.com>
2025-07-12 17:02:02 +0530

    Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-// next.config.mjs
-const isGithubPages = process.env.NODE_ENV === 'production';
-const repoName = 'your-repo-name'; // 🔁 replace with your repo name
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ⬅️ required for static export
-  trailingSlash: true,
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
